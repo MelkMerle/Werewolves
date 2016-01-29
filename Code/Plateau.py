8cfac39d0cc8cafb472ea612c07c141e14339b58
Melchior <melchior.merlin@student.ecp.fr>
2016-01-29 11:50:09 +0100

    Plateau continued : initialisation de la map, mais pas encore la fonction sale des updates

--- Code/Plateau.py
+++ Code/Plateau.py
@@ -1,19 +1,29 @@
-import Groupe
+from group import Group
+
 
 class PlateauDeJeu :
+
     def __init__(self):
-        self.groupes = Groupe()[]
+        self.groupes = []
 
     def getGroup(self, x, y):
         for groupe in self.groupes:
-            if groupe.x=x && groupe.y=y:
+            if (groupe.x==x & groupe.y==y):
                 return groupe
         print("Aucun groupe n'a été trouvé par la méthode getGroup")
         return None
 
-    def addGroup(self,x,y):
-        self.groupes.append(Groupe(x,y))
+    def addGroup(self,x,y,effectif,espece):
+        nouveau = Group(x,y,effectif,espece)
+        self.groupes.append(nouveau)
+        return nouveau
 
-    def getHumans(self):
+    def getMembers(self, espece):
+        members =[]
+        for group in self.groupes:
+            if group.species == espece:
+                members.append(group)
+        return members
 
-    def getSpecies(self, species):
+
+
