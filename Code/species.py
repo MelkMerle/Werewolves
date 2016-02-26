c20f21708a6b0b9626b47ac7e18d13aa3a5d3849
antoineb19 <antoinebenoit@msn.com>
2016-02-26 09:00:57 +0100

    Added species.inverse

--- Code/species.py
+++ Code/species.py
@@ -7,3 +7,10 @@ class Species(Enum):
     human = 'human'
     werewolf = 'werewolf'
     vampire = 'vampire'
+
+    def inverse(self):
+    	if self.value == 'vampire':
+    		return Species.werewolf
+    	if self.value == 'werewolf':
+    		return Species.vampire
+    	return Species.human
---
+++ Code/test_species.py
@@ -0,0 +1,6 @@
+from species import Species
+
+print("Bonjour !")
+print(Species.human.inverse())
+print(Species.werewolf.inverse())
+print(Species.vampire.inverse())
