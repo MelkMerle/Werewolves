b790132af0dc485bb3c3b66b9175eb92f6189407
Benjamin Poilve <benjamin.poilve@student.ecp.fr>
2016-02-26 09:03:07 +0100

    Refact IA

--- Code/IA.py
+++ Code/IA.py
@@ -1,16 +1,14 @@
-from Plateau import PlateauDeJeu
+from species import Species
 import time
 
 
-class Intelligence (PlateauDeJeu):
+class Intelligence ():
 
-    def __init__(self,plateau):
-        self.mySpecie = 0
-        self.state = plateau    #le plateau (liste des groupes, lignes, colonnes), pourra eventuellement etre modifie
+    def __init__(self):
                                 #selon l'interface desiree par l'IA
         self.startTime = time.time()
         self.maxValue =10
-        
+		self.mySpecie=0
     
     def timeleft(self):
         return (5*60)-(time.time() - self.startTime)
@@ -19,41 +17,35 @@ class Intelligence (PlateauDeJeu):
         # here we do so mathematics voodoo
         return heuristics
 
-
     def enumeratePossibleMissions(state):
         #here we do the possibility elaging, basically, returning an array of mission
-        missionAssigement=[1]
-        return missionAssigement
+        missionArray=generate(state)
+		sortedMissionArray=[]
+		for mission in missionArray:
+				sortedMissionArray.append([mission,mission.calc_mark()])
+        sortedMissionArray.sort(key=lambda x: int(x[1]))
+		return sortedMissionArray[-5:]
+
+	def generate(state):
+
+
     
     def CalulateNextSate(mission, state):
         #here we calculate the nextstate, considering a specific mission set
-        if(mission==1):
-            #look for humans
-            foes= state.getMembers(state, 2)
-            #look where I am
-            me=state.getMembers(state,self.mySpecie = 0)
-            #now move towards closest human group
-            
-        
-            
+        state=mission.execute(state)   
         return nextState
     
-    
      #Principal function, returning the best possible mission set
-    def Choose(state,specie):
+    def Choose(state,self.mySpecie):
         allmission=[]
         for mission in enumeratePossibleMissions(state):
             missiontotest=[CalulateNextSate(mission, state),0]
-            missiontotest[1]=minmax(missiontotest[0],!specie,1)
+            missiontotest[1]=minmax(missiontotest[0],specie.invert(),1)
             allmission.append(missiontotest)
         if(specie!=self.mySpecie):
             return allmission.sort(key=lambda x: int(x[1]))[0][0]
         else:
             return allmission.sort(key=lambda x: int(x[1]))[len(allmission)][0]
-
-    
-    
-
 
     #Here state is the groups in the possible state, it totally define the game (!!not the real groups though)
     #specie=1 for vampire if its me, 0 for werewolves (just to use ! , I am that lazy)
@@ -65,7 +57,7 @@ class Intelligence (PlateauDeJeu):
             if(recursiveValue>self.maxValue):
                 missiontotest[1]=calculateHeuristics( missiontotest[0],specie)
             else:
-                missiontotest[1]=minmax(missiontotest[0],!specie,recursiveValue+1)
+                missiontotest[1]=minmax(missiontotest[0],specie.invert(),recursiveValue+1)
             allmission.append(missiontotest)
         if(specie!=self.mySpecie):
             return allmission.sort(key=lambda x: int(x[1]))[0][1]
