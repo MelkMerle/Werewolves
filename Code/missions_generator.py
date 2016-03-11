ae69ba675da0164eebf641e5c7b5d6b954f20117
aliceleducq <leducqalice@gmail.com>
2016-03-11 12:18:35 +0100

    Avancées sur modélisation du produit cartésien

--- Code/missions_generator.py
+++ Code/missions_generator.py
@@ -8,28 +8,34 @@ def enumerate_possible_missions(state, my_species):
     my_groups = state.getMembers(my_species)
     for groupMe in my_groups:
         possible_actions = enumerate_possible_actions(state, groupMe, my_species)
-        rate_simple = 0
-        rate_split = 0
-        action_simple = None
-        action_split = None
+        action_simple = [None, None]
+        action_split = [None, None]
+        rate_action_simple = [-1000, -1000]
+        rate_action_split = [-1000, -1000]
 
         for action in possible_actions[0]:
             rate = action.calc_mark(state)
-            if rate > rate_simple:
-                action_simple = action
-                rate_simple = rate
+            if (rate > rate_action_simple[0]):
+                action_simple [0]= action
+                rate_action_simple[0] = rate
+            elif (rate > rate_action_simple[1]):
+                action_simple [1]= action
+                rate_action_simple[1] = rate
 
         for actions in possible_actions[1]:
             rate = actions[0].calc_mark(state) + actions[1].calc_mark(state)
-            if rate > rate_split:
-                action_split = actions
-                rate_split = rate
+            if rate > rate_action_split[0]:
+                action_split[0] = actions
+                rate_action_split[0] = rate
+            elif rate > rate_action_split[1]:
+                action_split[1] = actions
+                rate_action_split[1] = rate
 
-        if rate_split > rate_simple:
-            mission_array.append(action_split[0])
-            mission_array.append(action_split[1])
-        else:
-            mission_array.append(action_simple)
+        merged_actions=action_simple + action_split
+        merged_rates=rate_action_simple + rate_action_split
+        merged_actions.sort(key=dict(zip(merged_actions, merged_rates)).get)
+        mission_array.append(merged_actions[-2:])
+
     newMission = Mission(mission_array);
     finalArray.append(newMission);
     return finalArray;
@@ -39,3 +45,13 @@ def enumerate_possible_missions(state, my_species):
         sortedMissionArray.append([mission,mission.calc_mark(state)])
     sortedMissionArray.sort(key=lambda x: int(x[1]))
     return sortedMissionArray[-5:]"""
+
+def product(*args, **kwds):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = map(tuple, args) * kwds.get('repeat', 1)
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
--- Code/test_ia_enumeration_mission.py
+++ Code/test_ia_enumeration_mission.py
@@ -1,4 +1,4 @@
-from species import Species
+"""from species import Species
 from Plateau import PlateauDeJeu
 from group import Group
 from missions_generator import enumerate_possible_missions
@@ -21,3 +21,40 @@ for action in missionArray:
     assigned = action.assignedGroup
     target = action.target_group
     print("Action de type " + str(mission_type) + " : " + str(assigned) + " vers " + str(target))
+
+"""
+l1=[1,3,5]
+l2=[2,4,7]
+l3=sorted(l1 + l2)
+
+"""def product(*args, **kwds):
+    # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
+    # product(range(2), repeat=3) --> 000 001 010 011 100 101 110 111
+    pools = map(tuple, args) * kwds.get('repeat', 1)
+    result = [[]]
+    for pool in pools:
+        result = [x+[y] for x in result for y in pool]
+    for prod in result:
+        yield tuple(prod)
+"""
+
+"""def product(*args, **kwds):
+    "Alternative fast implementation of product for python < 2.6"
+    def cycle(sequence, uplevel):
+        while True:
+            vals = next(uplevel)   # advance upper level, raises if done
+            it = iter(sequence)    # (re-)start iteration of current level
+            try:
+                while True: yield vals + (next(it),)
+            except StopIteration:
+                pass
+
+    step = iter(((),))
+    for pool in map(tuple, args)*kwds.get('repeat', 1):
+        step = cycle(pool, step)   # build stack of iterators
+    return step
+
+L=product('ABCD','123')"""
+
+print([[a, b, c] for a in [1,2,3] for b in ['a','b'] for c in [4,5]])
+
