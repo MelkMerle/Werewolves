f738825eeae31e9832a728e5f44ae30dfd94d185
aliceleducq <leducqalice@gmail.com>
2016-03-13 21:51:51 +0100

    enumerate_possible_missions commenté proprement

--- Code/missions_generator.py
+++ Code/missions_generator.py
@@ -14,6 +14,7 @@ def enumerate_possible_missions(state, my_species):
         rate_action_split = [-1000, -1000]
 
         for action in possible_actions[0]:
+            #on gère les actions "simples": un groupe attaque un groupe d'humains
             rate = action.calc_mark(state)
             if (rate > rate_action_simple[0]):
                 action_simple [0]= action
@@ -23,6 +24,7 @@ def enumerate_possible_missions(state, my_species):
                 rate_action_simple[1] = rate
 
         for actions in possible_actions[1]:
+            #on gère les missions splittés
             rate = actions[0].calc_mark(state) + actions[1].calc_mark(state)
             if rate > rate_action_split[0]:
                 action_split[0] = actions
@@ -30,10 +32,12 @@ def enumerate_possible_missions(state, my_species):
             elif rate > rate_action_split[1]:
                 action_split[1] = actions
                 rate_action_split[1] = rate
+        #contournement de l'impossibilité de trier une liste de liste. estion de l'exception des actions splittées
         split1=action_split[0]
         split2=action_split[1]
         merged_actions=action_simple + [1] + [2]
         merged_rates=rate_action_simple + rate_action_split
+        #classement des actions pour un groupe
         merged_actions.sort(key=dict(zip(merged_actions, merged_rates)).get, reverse=True)
         index1=merged_actions.index(1)
         merged_actions[index1]=split1[0]
@@ -47,16 +51,11 @@ def enumerate_possible_missions(state, my_species):
             mission_array.append((merged_actions[-1:]))
         else:
             mission_array.append(merged_actions[-2:])
+            #on renvoie le top 2 des meilleures actions de chaque groupe
 
-
+    #on sort toutes le combinaisons possibles
     newMix=list(itertools.product(*mission_array))
     for mission in newMix:
         newMission = Mission(mission);
         finalArray.append(newMission);
     return finalArray;
-
-    """sortedMissionArray=[]
-    for mission in mission_array:
-        sortedMissionArray.append([mission,mission.calc_mark(state)])
-    sortedMissionArray.sort(key=lambda x: int(x[1]))
-    return sortedMissionArray[-5:]"""
