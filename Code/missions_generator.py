dca071f2a2e9c6584a190a796c7c17fbcc702e9a
antoineb19 <antoinebenoit@msn.com>
2016-03-14 18:42:36 +0100

    pb des actions split pas rassemblées

--- Code/missions_generator.py
+++ Code/missions_generator.py
@@ -32,28 +32,44 @@ def enumerate_possible_missions(state, my_species):
         merged_actions.sort(key=dict(zip(merged_actions, merged_rates)).get, reverse=True)
         if len(merged_actions)>(nb_human_groups*2):
             merged_actions=merged_actions[:(nb_human_groups*2)]
-        for element in merged_actions:
-            if element in split_list_index:
-                newIndex=merged_actions.index(element)
-                merged_actions[newIndex] = possible_split_actions[element][0]
-                merged_actions.insert(newIndex, possible_split_actions[element][1])
+
+        # Cette partie n'a pas sa place ici, tant qu'on n'a pas fait le product on garde les split actions ensemble
+        # for element in merged_actions:
+        #     if element in split_list_index:
+        #         newIndex=merged_actions.index(element)
+        #         merged_actions[newIndex] = possible_split_actions[element][0]
+        #         merged_actions.insert(newIndex, possible_split_actions[element][1])
+
+        # Troncature
+        # Pourquoi pas mettre le nombre maximum en paramètre
         if len(merged_actions)>(nb_human_groups*2):
             actions_array.append(merged_actions[:(nb_human_groups*2)])
         else:
             actions_array.append(merged_actions)
 
 
-    #on sort toutes le combinaisons possibles
-    newMix=list(itertools.product(*actions_array))
+    #on génère le produit cartésien
+    possible_missions=list(itertools.product(*actions_array))
+
     rate_missions=[]
     saved_missions=[]
-    for mission in newMix:
+
+
+    for mission_tuple in possible_missions:
+        mission = list(mission_tuple)
+
+        # On remplace par les index des split actions par les split actions en questions
+        index = -1
         for element in mission:
-            if type(element) is list:
-                index = mission.index(element)
-                element = element[0]
-                mission.insert(index,element[1])
-        #On vérifie qu'il n'y a pas de doublons dans les action.targetgroup
+            index += 1
+            if type(element) is int:
+                index_in_split_list = element
+                mission[index] = possible_split_actions[index_in_split_list][0]
+                mission.insert(index, possible_split_actions[index_in_split_list][1])
+
+
+        # On vérifie qu'il n'y a pas de doublons dans les action.targetgroup
+        # auquel cas on fait quoi ??
 
         target_list=[]
         duplicate_count=0
@@ -62,10 +78,13 @@ def enumerate_possible_missions(state, my_species):
                 target_list.append(action.target_group)
             else:
                 duplicate_count+=1
+
         if duplicate_count==0:
             newMission = Mission(mission)
             saved_missions.append(newMission)
             rate_missions.append(newMission.calc_mark(state))
+        # else what ? todo
+
     saved_missions.sort(key=dict(zip(saved_missions, rate_missions)).get, reverse=True)
     saved_missions=saved_missions[:facteur_brch_max]
     return saved_missions
--- Code/test_ia_enumeration_mission.py
+++ Code/test_ia_enumeration_mission.py
@@ -18,13 +18,13 @@ plateau.addThisGroup(myGroup)
 
 missionArray = enumerate_possible_missions(plateau, Species.werewolf)
 
-print(missionArray)
-print(len(missionArray))
-
 for mission in missionArray:
-    print("this is a mission")
+    print("\nthis is a mission")
+    print(mission)
+    # coup = mission.calculateCoup(plateau)
     for action in mission.actions:
-        action_type = action.action_type
-        assigned = action.assignedGroup
-        target = action.target_group
-        print("Action de type " + str(action_type) + " : " + str(assigned) + " vers " + str(target))
+        print(action)
+        # action_type = action.action_type
+        # assigned = action.assignedGroup
+        # target = action.target_group
+        # print("Action de type " + str(action_type) + " : " + str(assigned) + " vers " + str(target))
