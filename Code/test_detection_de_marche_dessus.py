839f42e2f2c590a966af09444d29ea23455c98ea
antoineb19 <antoinebenoit@msn.com>
2016-03-15 16:41:35 +0100

    test marche dessus

--- Code/test_detection_de_marche_dessus.py
+++ Code/test_detection_de_marche_dessus.py
@@ -6,15 +6,18 @@ from missions_generator import enumerate_possible_missions
 plateau = PlateauDeJeu(5, 5)
 
 
-plateau.addGroup(2,2,4,Species.vampire)
+plateau.addGroup(0,2,4,Species.vampire)
 plateau.addGroup(1,2,4,Species.vampire)
 plateau.addGroup(4,2,3,Species.human)
 plateau.addGroup(3,2,3,Species.human)
 plateau.addGroup(1,4,10,Species.werewolf)
 
+plateau.print()
 
 missionArray = enumerate_possible_missions(plateau, Species.vampire)
 
 for mission in missionArray:
     print("\n\nNOUVELLE MISSION, note : ", mission.calc_mark(plateau))
+    print(mission)
     coup = mission.calculateCoup(plateau)
+
