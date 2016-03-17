d461c4fc9b6cf6d0a11c23adc42ae51cd51de580
Melchior <melchior.merlin@student.ecp.fr>
2016-03-17 17:11:18 +0100

    timing qui marche et qui fonctionne dans les temps sur la map elinfierno

--- Code/network_manager.py
+++ Code/network_manager.py
@@ -35,7 +35,7 @@ class NetworkManager:
                 if(isinstance(message, int)):
                     data = struct.pack('=B', message)
                 elif (isinstance(message, str)):
-                    data = bytes(message, 'utf-8')
+                    data = bytes(message)
                 elif (isinstance(message, list)):
                     for mess in message:
                         self.send(mess)
